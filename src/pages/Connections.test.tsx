85ea07d03eb0df62984123fdeed8f74d0a332473
Steve Peak <steve@stevepeak.net>
2025-06-02 15:50:39 -0600

    upgrades

--- src/pages/Connections.test.tsx
+++ src/pages/Connections.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import { MemoryRouter } from "react-router-dom";
